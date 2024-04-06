88ce33019693b1b581595bfb4c61db152f040ab9
Matteo <matteo.morari04@gmail.com>
2024-04-06 15:27:23 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import it.kibo.fp.lib.*;
+
+public class App {
+    public static void main(String[] args) {
+        double num = InputData.readDouble("Ciao");
+        System.out.println("Hello, World!");
+    }
+}
